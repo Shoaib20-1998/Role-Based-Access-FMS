817ac7c55e75df6ce11cca184ba5f97368015a0c
shoaib mansoori <shoaib.mansoori@albanero.io>
2025-06-19 19:14:36 +0530

    re-factor-ci-file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,9 @@
-
-import React from 'react';
+import React from "react";
 function App() {
-    let data = 'aa'
+  let data = "aa";
 
+  data = 5;
 
-
-
-
-
-    
-    return <h1 className="bg-blue-400 h-5">sdfdsfdsfdsfdsf</h1>;
+  return <h1 className="bg-blue-400 h-5">sdfdsfdsfdsfdsf</h1>;
 }
-export default App
+export default App;
