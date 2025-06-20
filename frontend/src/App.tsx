5f423a1ea61112d07be907ec4bad453645a1c39f
shoaib mansoori <shoaib.mansoori@albanero.io>
2025-06-20 21:09:06 +0530

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,6 @@ import React from "react";
 function App() {
   let data = "aa";
 
-  return <h1 className="bg-blue-400 h-5">shoaib mansuri again testttt</h1>;
+  return <h1 className="bg-blue-400 h-5">shoaib mansuri again testtttsdfdsfsdf</h1>;
 }
 export default App;
