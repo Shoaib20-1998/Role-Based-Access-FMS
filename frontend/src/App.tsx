da041af801c9fefce6fc37a842f7282ae1a6fe27
shoaib mansoori <shoaib.mansoori@albanero.io>
2025-06-20 16:18:24 +0530

    integrated cd to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,6 @@ import React from "react";
 function App() {
   let data = "aa";
 
-  return <h1 className="bg-blue-400 h-5">sdfdsfdsfdsfdsf</h1>;
+  return <h1 className="bg-blue-400 h-5">shoaib mansuri</h1>;
 }
 export default App;
